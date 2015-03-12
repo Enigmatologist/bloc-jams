7543159ec16b44b8ef274bf1bfc0cf6da898fb9d
Toby T Castro <tc6586@gmail.com>
2015-03-12 21:33:13 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,13 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
     controller: 'Collection.controller',
     templateUrl: '/templates/collection.html'
   });
+
+  $stateProvider.state('album', {
+    url: '/album',
+    templateUrl: '/templates/album.html',
+    controller: 'Album.controller'
+  });
+
 }]);
  
 
@@ -63,4 +70,62 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
     for (var i = 0; i < 33; i++) {
       $scope.albums.push(angular.copy(albumPicasso));
     }
-}]);      
+}]); 
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
+
+      var hoveredSong = null;
+      var playingSong = null;
+
+      $scope.onHoverSong = function(song){
+        hoveredSong = song;
+      };
+
+      $scope.offHoverSong = function(song){
+        hoveredSong = null;
+      };
+
+        $scope.getSongState = function(song){
+          if(song === playingSong){
+            return 'playing';
+          }
+          else if(song === hoveredSong){
+            return 'hovered';
+          }
+          return 'default';
+          };
+
+            $scope.playSong = function(song){
+              playingSong = song;
+            };
+
+            $scope.pauseSong = function(song){
+              playingSong = null;
+            };
+}]);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
